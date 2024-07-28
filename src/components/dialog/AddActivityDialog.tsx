46fc126240a19f93db4c3edfc4ecbc1f99b9cfb4
Kastro12 <aleksandarrlm@gmail.com>
2024-07-28 23:35:31 +0200

    eslint error fixed

--- src/components/dialog/AddActivityDialog.tsx
+++ src/components/dialog/AddActivityDialog.tsx
@@ -73,7 +73,7 @@ export default function AddActivityDialog() {
         </IconButton> */}
         <DialogContent>
           <Typography variant='body1' sx={{ mb: '9px', fontSize: '16px' }}>
-            The dates you've selected result in a{' '}
+            The dates you&apos;ve selected result in a{' '}
             <strong>{confirmAddingActivity?.differenceBetweenStartFinishDate}-day</strong> vacation.
           </Typography>
           <Typography variant='body1' sx={{ mb: '24px', fontSize: '16px' }}>
