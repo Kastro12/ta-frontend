258ba26522ad3fa9b8f335a920ca14be35871d3d
Kastro12 <aleksandarrlm@gmail.com>
2024-09-10 14:10:02 +0200

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  return NextResponse.next();
+}
+
 // Define which paths to apply the middleware to
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
